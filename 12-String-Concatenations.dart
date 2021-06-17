d8df9cd5aa2b37dc66e7a0f6ad90513a0dbe887e
DON C JOHN <doncjohn.dcj@gmail.com>
2021-06-17 15:29:56 +0530

    Create 12-String-Concatenations.dart

---
+++ 12-String-Concatenations.dart
@@ -0,0 +1,10 @@
+void main() {
+String name = 'Don';
+var s1 = "My name is Don";
+var s2 = "My name is $name";
+var s3 = "My name is "+"Don";
+var s4 = "My name is ""Don";
+var s5 = "My name is " + name;
+var s6 = "My name is ${name.toUpperCase()}”;
+var s6 = "My name is ${name.toLowerCase()}”;
+}
