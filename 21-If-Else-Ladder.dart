b6bcb22030515bc475f1e26e82a2bc74eaefbcc3
DON C JOHN <doncjohn.dcj@gmail.com>
2021-06-18 12:25:36 +0530

    Create 21-If-Else-Ladder.dart

---
+++ 21-If-Else-Ladder.dart
@@ -0,0 +1,14 @@
+void main()
+{
+    var a = 10;
+    var b = 15;
+     if(a > b){
+    print("a is greater than b");
+    }
+    else if(a == b){
+      print("a and b are equal");
+    }
+    else {
+    print("b is greater than a");
+    } 
+}
