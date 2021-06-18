18631ff83d55cbcb0aae405a19e4a20a35acc5fa
DON C JOHN <doncjohn.dcj@gmail.com>
2021-06-18 12:16:16 +0530

    Create 18-Basic-of-Maps.dart

---
+++ 18-Basic-of-Maps.dart
@@ -0,0 +1,15 @@
+void main() {  
+var prizes = {
+// Key : Value
+  'first': 'Don',
+  'second': 'Albin',
+  'third': 'Akku'
+};
+prizes['fourth'] = 'Maritta';
+print(prizes);
+var nobleGases = Map<int, String>();
+nobleGases[2]   = 'helium';
+nobleGases[10] = 'neon';
+nobleGases[18] = 'argon';
+print(nobleGases);
+}
