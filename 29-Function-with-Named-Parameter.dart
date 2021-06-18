eae85ee5def72dce6bf462db855be2b93bc6b648
DON C JOHN <doncjohn.dcj@gmail.com>
2021-06-18 12:33:27 +0530

    Create 29-Function-with-Named-Parameter.dart

---
+++ 29-Function-with-Named-Parameter.dart
@@ -0,0 +1,8 @@
+void main() {  
+ describe( name:"Don", age:23, height:5.9);
+}
+void describe ({String name,int age, double height}){
+  print("Hello my name is $name");
+  print("Hello my name is $age");
+  print("Hello my name is $height");
+}
