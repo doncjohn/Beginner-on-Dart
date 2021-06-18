9a4cc70660b7c4ed292d1a8b29193e16cb8908eb
DON C JOHN <doncjohn.dcj@gmail.com>
2021-06-18 12:32:30 +0530

    Create 28-Functions.dart

---
+++ 28-Functions.dart
@@ -0,0 +1,9 @@
+void main() {  
+ describe("Don",23,5.9);
+ describe("Akku",23); 
+}
+void describe (String name,int age, [double height =  0.0] ){
+  print("Hello my name is $name");
+  print("Hello my name is $age");
+  print("Hello my name is $height");
+}
